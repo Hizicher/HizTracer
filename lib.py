08a308d5de783136e6a6f8c57e8d902ff67f13a7
aeozbek <aeozbek@kth.se>
2025-12-25 16:14:10 +0100

    Closes #1 : Template for setting up the canvas is ready

--- lib.py
+++ lib.py
@@ -1,27 +1,41 @@
+from PIL import Image
+import os
+
 class Vector:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, z):
         self.x = x
         self.y = y
+        self.z = z
     
 
     def __iadd__(self, other): # These methods are defining addition, multiplication and division with vectors, where division and multiplication ofcourse occurs with scalars
 
         self.x += other.x
-        self.y += other.y 
+        self.y += other.y
+        self.z += other.z 
 
         return self
     
     def __truediv__(self, other):
         new_x = self.x / other
         new_y = self.y / other
+        new_z = self.z / other
 
-        return Vector(new_x, new_y)
+        return Vector(new_x, new_y, new_z)
 
     def __mul__(self, other):
         new_x = self.x * other
         new_y = self.y * other
+        new_z = self.z * other
 
-        return Vector(new_x, new_y)
+        return Vector(new_x, new_y, new_z)
     
+class Window:
 
+    def __init__(self, size_x, size_y):
+        
+
+def blit_image():
+
+    pass
